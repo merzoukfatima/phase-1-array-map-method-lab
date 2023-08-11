1d26a02a4bb605070aaaae75241dd34cf84d1f14
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-08-11 02:43:57 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((words) => {
+    const array = words.split(" ");
+    const new_array = array.map(
+      (item) => item.charAt(0).toUpperCase() + item.slice(1)
+    );
+    return new_array.join(" ");
+  });
+};
